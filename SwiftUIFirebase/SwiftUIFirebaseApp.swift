0d505d2540c08c358da3c112258307b6fab3c990
Kirill <frolkv@gmail.com>
2023-03-14 21:02:39 +0300

    Initial Commit

---
+++ SwiftUIFirebase/SwiftUIFirebaseApp.swift
@@ -0,0 +1,30 @@
+//
+//  SwiftUIFirebaseApp.swift
+//  SwiftUIFirebase
+//
+//  Created by Kirill Frolovskiy on 14.03.2023.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct SwiftUIFirebaseApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("Configured Firebase!")
+        
+        return true
+    }
+}
