c7ef64ca0c0a4e9baac20a3dad9aa8ce20501834
Kirill <frolkv@gmail.com>
2023-04-21 21:07:06 +0300

    feat: add Google Sing up

--- SwiftUIFirebase/Authentication/AuthenticationManager.swift
+++ SwiftUIFirebase/Authentication/AuthenticationManager.swift
@@ -34,38 +34,61 @@ final class AuthenticationManager {
         
     }
     
-    @discardableResult
-    func createUser(email: String, password: String) async throws -> AuthDataResultModel {
-        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
-        return AuthDataResultModel(user: authDataResult.user)
-    }
-    
-    @discardableResult
-    func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
-        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
-        return AuthDataResultModel(user: authDataResult.user)
-    }
-    
-    func resetPassword(email: String) async throws {
-        try await Auth.auth().sendPasswordReset(withEmail: email)
-    }
-    
-    func updatePassword(password: String) async throws {
-        guard let user = Auth.auth().currentUser else {
-            throw URLError(.badServerResponse)
-        }
-        try await user.updatePassword(to: password)
-    }
-    
-    func updateEmail(email: String) async throws {
-        guard let user = Auth.auth().currentUser else {
-            throw URLError(.badServerResponse)
-        }
-        try await user.updateEmail(to: email)
-    }
+
     
     func signOut() throws {
         try Auth.auth().signOut()
     }
     
+}
+
+
+//MARK: SIGN IN EMAIL
+extension AuthenticationManager {
+
+  @discardableResult
+  func createUser(email: String, password: String) async throws -> AuthDataResultModel {
+      let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+      return AuthDataResultModel(user: authDataResult.user)
+  }
+
+  @discardableResult
+  func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
+      let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
+      return AuthDataResultModel(user: authDataResult.user)
+  }
+
+  func resetPassword(email: String) async throws {
+      try await Auth.auth().sendPasswordReset(withEmail: email)
+  }
+
+  func updatePassword(password: String) async throws {
+      guard let user = Auth.auth().currentUser else {
+          throw URLError(.badServerResponse)
+      }
+      try await user.updatePassword(to: password)
+  }
+
+  func updateEmail(email: String) async throws {
+      guard let user = Auth.auth().currentUser else {
+          throw URLError(.badServerResponse)
+      }
+      try await user.updateEmail(to: email)
+  }
+}
+
+//MARK: SIGN IN SSO
+extension AuthenticationManager {
+
+  @discardableResult
+  func singInWithGoogle(tokens: GoogleSingInResultModel) async throws -> AuthDataResultModel {
+    let credential = GoogleAuthProvider.credential(withIDToken: tokens.idToken, accessToken: tokens.accessToken)
+    return try await singIn(credential: credential)
+  }
+
+  func singIn(credential: AuthCredential) async throws -> AuthDataResultModel {
+    let authDataResult = try await Auth.auth().signIn(with: credential)
+    return AuthDataResultModel(user: authDataResult.user)
+  }
+
 }
--- SwiftUIFirebase/Authentication/AuthenticationView.swift
+++ SwiftUIFirebase/Authentication/AuthenticationView.swift
@@ -6,38 +6,79 @@
 //
 
 import SwiftUI
+import GoogleSignIn
+import GoogleSignInSwift
+import FirebaseAuth
+
+struct GoogleSingInResultModel {
+  let idToken: String
+  let accessToken: String
+}
+
+final class AutheticationViewModel: ObservableObject {
+
+  func singInGoogle() async throws {
+    guard let topVC = await Utilites.shared.topViewController() else {
+      throw URLError(.cannotFindHost)
+    }
+    let gidSingInResult = try await GIDSignIn.sharedInstance.signIn(withPresenting: topVC)
+    //    gidSingInResult.user
+
+    guard let idToken = gidSingInResult.user.idToken?.tokenString else {
+      throw URLError(.badServerResponse)
+    }
+    let accessToken = gidSingInResult.user.accessToken.tokenString
+
+    let tokens = GoogleSingInResultModel(idToken: idToken, accessToken: accessToken)
+    try await AuthenticationManager.shared.singInWithGoogle(tokens: tokens)
+
+  }
+}
+
 
 struct AuthenticationView: View {
-    
-    @Binding var showSignInView: Bool
-    
-    var body: some View {
-        
-        VStack {
-            
-            NavigationLink {
-                SignInEmailView(showSignView: $showSignInView)
-            } label: {
-                Text("Sing In with Email")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .frame(height: 55)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-            }
-            Spacer()
+
+  @StateObject private var viewModel = AutheticationViewModel()
+  @Binding var showSignInView: Bool
+
+  var body: some View {
+
+    VStack {
+
+      NavigationLink {
+        SignInEmailView(showSignView: $showSignInView)
+      } label: {
+        Text("Sing In with Email")
+          .font(.headline)
+          .foregroundColor(.white)
+          .frame(height: 55)
+          .frame(maxWidth: .infinity)
+          .background(Color.blue)
+          .cornerRadius(10)
+      }
+
+      GoogleSignInButton(viewModel: GoogleSignInButtonViewModel(scheme: .light, style: .wide, state: .normal)) {
+        Task {
+          do {
+            try await viewModel.singInGoogle()
+            showSignInView = false
+          } catch {
+            print(error)
+          }
         }
-        .padding()
-        .navigationTitle("Sign In")
+      }
+      Spacer()
     }
+    .padding()
+    .navigationTitle("Sign In")
+  }
 }
 
 struct AuthenticationView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationStack {
-            
-            AuthenticationView(showSignInView: .constant(false))
-        }
+  static var previews: some View {
+    NavigationStack {
+
+      AuthenticationView(showSignInView: .constant(false))
     }
+  }
 }
